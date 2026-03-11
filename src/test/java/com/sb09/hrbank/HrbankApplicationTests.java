011aac524f8ffe04d53f5b351a0748d154317d8e
윤성준 <jseong013@gmail.com>
2026-03-11 16:29:56 +0900

    chore: initialize project

---
+++ src/test/java/com/sb09/hrbank/HrbankApplicationTests.java
@@ -0,0 +1,14 @@
+package com.sb09.hrbank;
+
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.Disabled;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@Disabled("Enable after DB configuration is added")
+@SpringBootTest
+class HrbankApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+}
