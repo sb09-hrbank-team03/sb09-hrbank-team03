b83dc846207b094ba19f62ebdd64cb4eb8f02956
전명훈 <acekid2120@gmail.com>
2026-03-12 14:52:31 +0900

    부서 관리 패키지 구조 생성 및 git ignore 최적화

---
+++ src/main/java/com/sb09/hrbank/entity/Department.java
@@ -0,0 +1,14 @@
+package com.sb09.hrbank.entity;
+
+
+import jakarta.persistence.Entity;
+import lombok.AccessLevel;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Getter
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+public class Department extends BaseEntity {
+
+}
