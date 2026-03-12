e344fe3f6b7e58093c97c24597fe7ed1de5f3fd8
윤성준 <jseong013@gmail.com>
2026-03-12 18:38:01 +0900

    feat: 직원 등록 컨트롤러 구현 및 예외 처리, 부서 어노테이션 수정

--- src/main/java/com/sb09/hrbank/controller/EmployeeController.java
+++ src/main/java/com/sb09/hrbank/controller/EmployeeController.java
@@ -1,5 +1,26 @@
 package com.sb09.hrbank.controller;
 
+import com.sb09.hrbank.dto.request.EmployeeCreateRequest;
+import com.sb09.hrbank.dto.response.EmployeeDto;
+import com.sb09.hrbank.service.EmployeeService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseStatus;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api/employees")
+@RequiredArgsConstructor
 public class EmployeeController {
 
+  private final EmployeeService employeeService;
+
+  @PostMapping
+  @ResponseStatus(HttpStatus.CREATED)
+  public EmployeeDto create(@RequestBody EmployeeCreateRequest request) {
+    return employeeService.create(request);
+  }
 }
--- src/main/java/com/sb09/hrbank/exception/GlobalExceptionHandler.java
+++ src/main/java/com/sb09/hrbank/exception/GlobalExceptionHandler.java
@@ -6,9 +6,9 @@ import java.util.NoSuchElementException;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ExceptionHandler;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
 
-@RestController
+@RestControllerAdvice
 public class GlobalExceptionHandler {
 
   @ExceptionHandler(IllegalArgumentException.class)
