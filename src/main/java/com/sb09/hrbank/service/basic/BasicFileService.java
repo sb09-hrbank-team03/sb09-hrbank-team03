d306c0dc7cd11db90ed5b05a384682d463a4a15a
apple <apple@apples-MacBook-Air.local>
2026-03-12 22:58:08 +0900

    feat: 파일 관리 기능 구현 및 mapstruct 의존성 추가

---
+++ src/main/java/com/sb09/hrbank/service/basic/BasicFileService.java
@@ -0,0 +1,25 @@
+package com.sb09.hrbank.service.basic;
+
+import com.sb09.hrbank.entity.FileMeta;
+import com.sb09.hrbank.repository.FileMetaRepository;
+import com.sb09.hrbank.service.FileService;
+import com.sb09.hrbank.storage.FileStorage;
+import java.util.NoSuchElementException;
+import lombok.RequiredArgsConstructor;
+import org.springframework.core.io.Resource;
+import org.springframework.stereotype.Service;
+
+@Service
+@RequiredArgsConstructor
+public class BasicFileService implements FileService {
+
+  private final FileMetaRepository repository;
+  private final FileStorage storage;
+
+  @Override
+  public Resource download(Long id) {
+    FileMeta file = repository.findById(id)
+        .orElseThrow(() -> new NoSuchElementException("파일 없음"));
+    return storage.load(file.getPath());
+  }
+}
---
+++ src/main/java/com/sb09/hrbank/storage/FileStorage.java
@@ -0,0 +1,13 @@
+package com.sb09.hrbank.storage;
+
+import org.springframework.core.io.FileSystemResource;
+import org.springframework.core.io.Resource;
+import org.springframework.stereotype.Component;
+
+@Component
+public class FileStorage {
+
+  public Resource load(String path) {
+    return new FileSystemResource(path);
+  }
+}
