44b773441fe2a3e5cfde19ca58fab0bac2650921
전명훈 <acekid2120@gmail.com>
2026-03-13 11:26:04 +0900

    구현: DepartmentRepository 기본 구현

--- src/main/java/com/sb09/hrbank/repository/DepartmentRepository.java
+++ src/main/java/com/sb09/hrbank/repository/DepartmentRepository.java
@@ -1,8 +1,12 @@
 package com.sb09.hrbank.repository;
 
 import com.sb09.hrbank.entity.Department;
+import java.util.List;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface DepartmentRepository extends JpaRepository<Department, Long> {
 
+  boolean existsByName(String name);
+
+  List<Department> findByNameContainingOrDescriptionContaining(String name, String description);
 }
