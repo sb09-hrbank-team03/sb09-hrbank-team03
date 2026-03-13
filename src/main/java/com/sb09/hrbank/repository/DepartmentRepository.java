1b39135d6590f63ccb52267cc56d0d0f8093840a
전명훈 <acekid2120@gmail.com>
2026-03-13 09:45:00 +0900

    수정: main 브랜치 최신화 반영

---
+++ src/main/java/com/sb09/hrbank/repository/DepartmentRepository.java
@@ -0,0 +1,8 @@
+package com.sb09.hrbank.repository;
+
+import com.sb09.hrbank.entity.Department;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface DepartmentRepository extends JpaRepository<Department, Long> {
+
+}
