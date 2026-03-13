fad7dbebaca1c7c2029e77d959a2eac86d9ff06b
nano <152370024+nano-study@users.noreply.github.com>
2026-03-13 10:13:49 +0900

    구현: 파일 관리 기능 구현 및 mapstruct 의존성 추가 (#6)

---
+++ src/main/java/com/sb09/hrbank/entity/FileMeta.java
@@ -0,0 +1,28 @@
+package com.sb09.hrbank.entity;
+
+import com.sb09.hrbank.entity.base.BaseUpdatableEntity;
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.Table;
+import lombok.AccessLevel;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Getter
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@Table(name = "binary_contents")
+public class FileMeta extends BaseUpdatableEntity {
+
+  @Column(name = "file_name", nullable = false)
+  private String fileName;
+
+  @Column(nullable = false)
+  private Long size;
+
+  @Column(name = "content_type", nullable = false)
+  private String contentType;
+
+  @Column(nullable = false)
+  private String path;
+}
