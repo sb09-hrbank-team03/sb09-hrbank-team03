1b05149fb652601b9387e25d5994976837e92790
ColorMarker <AAAstudying0987@gmail.com>
2026-03-13 14:02:00 +0900

    구현: 이력 정보 목록 조회 기능 구현 (#10)
    * 부서 관리 패키지 구조 생성 및 git ignore 최적화
    
    * 구현 및 수정: 공통 에러 처리 및 baseEntity 방식으로 수정
    
    * 수정: employee 엔티티 baseUpdatableEntity extend 방식으로 변경
    
    * 추가: 직원 수정 이력 관련 entity와 레포지토리 설정 완료
    
    * 수정: 일부 엔티티 수정 및 yaml 파일 db 연결 설정 추가
    
    * 구현: 부서 관리 패키지 구조 생성 및 git ignore 최적화
    
    * 구현: 부서 엔티티 식별자 타입 변경(Long) 및 필드 완성
    
    * 기타: merge 전 중간 저장
    
    * 기타: merge 전 중간 저장
    
    * feat: 직원 등록 요청 dto 추가
    
    * feat: 직원 등록 API 기본 구조 구현 및 로직 구현
    
    * feat: 직원 등록 컨트롤러 구현 및 예외 처리, 부서 어노테이션 수정
    
    * 추가: controller 기본 골자 작성 및 service 함수 구상 중
    
    * feat: 파일 관리 기능 구현 및 mapstruct 의존성 추가
    
    * 수정: main 브랜치 최신화 반영
    
    * 수정: BaseEntity의 id값 존재로, DepartmentEntity ãid값 삭제
    
    * 구현: changeLogController 구조 완성 및 request를 받기 위한 dto 생성, cursorPageResponse 생성
    
    * 기타: merge 처리 완료
    
    * 수정: Department entity에서 extends를 BaseUpdatableEntity로 변경
    
    * 구현: DepartmentRepository 기본 구현
    
    * 구현: 이력 받아오는 서비스 구현 완료. 레포지토리 함수 생성 완료
    
    * 수정: DepartmentRepository 쿼리ë부서 목록 조회 메서드 수정
    
    * 기타: merge 전 확인

--- src/main/java/com/sb09/hrbank/service/basic/BasicChangeLogService.java
+++ src/main/java/com/sb09/hrbank/service/basic/BasicChangeLogService.java
@@ -1,28 +1,88 @@
 package com.sb09.hrbank.service.basic;
 import com.sb09.hrbank.dto.common.CursorPageResponse;
 import com.sb09.hrbank.dto.request.ChangeLogListRequest;
+import com.sb09.hrbank.dto.request.EmployeeUpdateRequest;
 import com.sb09.hrbank.dto.response.ChangeLogDetailDto;
+import com.sb09.hrbank.dto.response.ChangeLogDto;
+import com.sb09.hrbank.entity.ChangeLog;
+import com.sb09.hrbank.entity.ChangeType;
+import com.sb09.hrbank.mapper.ChangeLogMapper;
+import com.sb09.hrbank.mapper.CursorPageResponseMapper;
+import com.sb09.hrbank.repository.ChangeLogDetailRepository;
+import com.sb09.hrbank.repository.ChangeLogRepository;
 import com.sb09.hrbank.service.ChangeLogService;
 import java.time.Instant;
+import java.time.temporal.ChronoUnit;
 import lombok.RequiredArgsConstructor;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.Slice;
+import org.springframework.data.domain.Sort;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 @Service
 @RequiredArgsConstructor
+@Transactional
 public class BasicChangeLogService implements ChangeLogService {
+  private final ChangeLogRepository changeLogRepository;
+  private final ChangeLogDetailRepository changeLogDetailRepository;
+  private final CursorPageResponseMapper cursorPageResponseMapper;
+  private final ChangeLogMapper changeLogMapper;
 
+  @Transactional(readOnly = true)
   @Override
   public Long getCount(Instant fromDate, Instant toDate) {
-    return 0L;
+    if(fromDate == null){
+      // 기본 일주일 전
+      fromDate = toDate.minus(7, ChronoUnit.DAYS);
+    }
+    if(toDate == null){
+      // 기본 오늘
+      toDate = Instant.now();
+    }
+    return changeLogRepository.countChangeLogByDuration(fromDate, toDate);
   }
 
+  @Transactional(readOnly = true)
   @Override
   public ChangeLogDetailDto getDetails(Long id) {
     return null;
   }
 
+  @Transactional(readOnly = true)
   @Override
-  public CursorPageResponse history(ChangeLogListRequest request) {
+  public CursorPageResponse<ChangeLogDto> history(ChangeLogListRequest request) {
+
+    Sort sort = Sort.by(Sort.Direction.fromString(request.getSortDirection()), request.getSortField())
+        .and(Sort.by(Sort.Direction.DESC, "id"));
+    Pageable pageable = PageRequest.of(0, request.getSize(), sort);
+    Slice<ChangeLog> logSlice = changeLogRepository.findAllByCondition(
+        request.getEmployeeNumber(),
+        request.getMemo(),
+        request.getIpAddress(),
+        request.getType(),
+        request.getAtFrom(),
+        request.getAtTo(),
+        request.getIdAfter(),
+        pageable
+    );
+
+    return cursorPageResponseMapper.fromSlice(
+        logSlice,
+        log -> changeLogMapper.toDto(log),
+        log -> {
+          if(request.getSortField().equals("ipAddress")){
+            return log.getIpAddress();
+          }
+          return log.getCreatedAt();
+        },
+          log -> log.getId()
+    );
+  }
+
+  @Override
+  public ChangeLog create(ChangeType changeType, Long id, EmployeeUpdateRequest request) {
     return null;
   }
 }
