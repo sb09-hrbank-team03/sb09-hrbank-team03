b46d72f5a423e59badada3a861db1ef9fd0b8c89
전명훈 <acekid2120@gmail.com>
2026-03-14 23:07:24 +0900

    구현: 부서 생성 API 컨트롤러 구현

---
+++ src/main/java/com/sb09/hrbank/controller/DepartmentController.java
@@ -0,0 +1,28 @@
+package com.sb09.hrbank.controller;
+
+import com.sb09.hrbank.dto.request.DepartmentCreateRequest;
+import com.sb09.hrbank.service.DepartmentService;
+import jakarta.validation.Valid;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseStatus;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api/departments")
+@RequiredArgsConstructor
+public class DepartmentController {
+
+  private final DepartmentService departmentService;
+
+  @PostMapping
+  @ResponseStatus(HttpStatus.CREATED)
+  public void createDepartment(
+      @Valid @RequestBody DepartmentCreateRequest request
+  ) {
+    departmentService.createDepartment(request);
+  }
+}
