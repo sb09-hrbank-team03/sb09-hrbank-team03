b51344257cdcaa257e02a5cc5a4da719234b9130
전명훈 <acekid2120@gmail.com>
2026-03-16 01:03:44 +0900

    개선: ãDepartment 생ìMapper로 DTO와 Entity간 변환 로직 분리

---
+++ src/main/java/com/sb09/hrbank/mapper/DepartmentMapper.java
@@ -0,0 +1,11 @@
+package com.sb09.hrbank.mapper;
+
+import com.sb09.hrbank.dto.request.DepartmentCreateRequest;
+import com.sb09.hrbank.entity.Department;
+import org.mapstruct.Mapper;
+
+@Mapper(componentModel = "spring")
+public interface DepartmentMapper {
+
+  Department toEntity(DepartmentCreateRequest request);
+}
