b89c518d75fb9b751864217717e1f1c40ed22fd0
apple <apple@apples-MacBook-Air.local>
2026-03-15 17:19:31 +0900

    feat: 백업 목록 조회 구현-QueryDSL

--- src/main/java/com/sb09/hrbank/repository/BackupRepository.java
+++ src/main/java/com/sb09/hrbank/repository/BackupRepository.java
@@ -4,8 +4,9 @@ import com.sb09.hrbank.entity.BackupHistory;
 import com.sb09.hrbank.entity.BackupStatus;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-public interface BackupRepository extends JpaRepository<BackupHistory, Long> {
+public interface BackupRepository
+    extends JpaRepository<BackupHistory, Long>,
+    BackupRepositoryCustom {
 
   boolean existsByBackupStatus(BackupStatus backupStatus);
-
 }
--- src/main/java/com/sb09/hrbank/service/basic/BasicBackupService.java
+++ src/main/java/com/sb09/hrbank/service/basic/BasicBackupService.java
@@ -1,14 +1,16 @@
 package com.sb09.hrbank.service.basic;
 
+import com.sb09.hrbank.dto.common.CursorPageResponse;
+import com.sb09.hrbank.dto.request.BackupListRequest;
 import com.sb09.hrbank.dto.response.BackupDto;
 import com.sb09.hrbank.entity.BackupHistory;
 import com.sb09.hrbank.entity.BackupStatus;
 import com.sb09.hrbank.mapper.BackupMapper;
+import com.sb09.hrbank.mapper.CursorPageResponseMapper;
 import com.sb09.hrbank.repository.BackupRepository;
 import com.sb09.hrbank.service.BackupService;
-import java.time.Instant;
 import lombok.RequiredArgsConstructor;
-import org.springframework.dao.DataIntegrityViolationException;
+import org.springframework.data.domain.*;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
@@ -18,27 +20,35 @@ public class BasicBackupService implements BackupService {
 
   private final BackupRepository backupRepository;
   private final BackupMapper backupMapper;
+  private final CursorPageResponseMapper cursorMapper;
 
   @Override
   @Transactional
   public BackupDto backup(String ip) {
 
-    validateBackupNotRunning();
-
-    BackupHistory history = BackupHistory.start(ip);
-
-    try {
-      BackupHistory saved = backupRepository.save(history);
-      return backupMapper.toDto(saved);
-
-    } catch (DataIntegrityViolationException e) {
-      throw new IllegalStateException("이미 진행중인 백업이 있습니다.");
-    }
-  }
-
-  private void validateBackupNotRunning() {
     if (backupRepository.existsByBackupStatus(BackupStatus.IN_PROGRESS)) {
       throw new IllegalStateException("이미 진행중인 백업이 있습니다.");
     }
+
+    BackupHistory history = BackupHistory.start(ip);
+
+    BackupHistory saved = backupRepository.save(history);
+
+    return backupMapper.toDto(saved);
+  }
+
+  @Override
+  @Transactional(readOnly = true)
+  public CursorPageResponse<BackupDto> getBackups(BackupListRequest request) {
+
+    Slice<BackupHistory> slice =
+        backupRepository.searchBackups(request);
+
+    return cursorMapper.fromSlice(
+        slice,
+        backupMapper::toDto,
+        BackupHistory::getStartedAt,
+        BackupHistory::getId
+    );
   }
 }
