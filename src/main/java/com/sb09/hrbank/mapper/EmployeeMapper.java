acbbfd33003d6edef06c200ef44f096465911c8d
윤성준 <jseong013@gmail.com>
2026-03-16 09:29:50 +0900

    refactor: EmployeeMapper 인터페이스로 변경 및 DTO 매핑 개선

--- src/main/java/com/sb09/hrbank/mapper/EmployeeMapper.java
+++ src/main/java/com/sb09/hrbank/mapper/EmployeeMapper.java
@@ -1,26 +1,14 @@
 package com.sb09.hrbank.mapper;
 
 import com.sb09.hrbank.dto.response.EmployeeDto;
-import com.sb09.hrbank.entity.Department;
 import com.sb09.hrbank.entity.Employee;
-import org.springframework.stereotype.Component;
+import org.mapstruct.Mapper;
+import org.mapstruct.Mapping;
 
-@Component
-public class EmployeeMapper {
+@Mapper(componentModel = "spring")
+public interface EmployeeMapper {
 
-  public EmployeeDto toDto(Employee employee) {
-    Department department = employee.getDepartment();
-    return new EmployeeDto(
-        employee.getId(),
-        employee.getName(),
-        employee.getEmail(),
-        employee.getEmployeeNumber(),
-        department.getId(),
-        department.getName(),
-        employee.getPosition(),
-        employee.getHireDate(),
-        employee.getStatus(),
-        employee.getProfileImageId()
-    );
-  }
+  @Mapping(target = "departmentId", source = "department.id")
+  @Mapping(target = "departmentName", source = "department.name")
+  EmployeeDto toDto(Employee employee);
 }
