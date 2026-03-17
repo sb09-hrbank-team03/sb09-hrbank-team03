7bf65ce7dd497f852d9f5b59a6d798e75bc653e4
전명훈 <acekid2120@gmail.com>
2026-03-17 16:52:50 +0900

    refactor: 미사용 import 제거

--- src/main/java/com/sb09/hrbank/service/DepartmentService.java
+++ src/main/java/com/sb09/hrbank/service/DepartmentService.java
@@ -5,7 +5,6 @@ import com.sb09.hrbank.dto.request.DepartmentSearchRequest;
 import com.sb09.hrbank.dto.response.DepartmentDto;
 import com.sb09.hrbank.dto.request.DepartmentCreateRequest;
 import com.sb09.hrbank.dto.request.DepartmentUpdateRequest;
-import java.util.List;
 
 public interface DepartmentService {
 
