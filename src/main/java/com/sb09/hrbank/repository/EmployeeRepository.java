dc4ab43624de1610d0b91a55d07fe0326c4de0c5
Color-Marker <AAAstudying0987@gmail.com>
2026-03-17 14:07:37 +0900

    extra: import하나 깜박해서 추가

--- src/main/java/com/sb09/hrbank/repository/EmployeeRepository.java
+++ src/main/java/com/sb09/hrbank/repository/EmployeeRepository.java
@@ -9,6 +9,8 @@ import java.util.List;
 import java.util.Optional;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
 public interface EmployeeRepository extends JpaRepository<Employee, Long>, EmployeeRepositoryCustom {
   boolean existsByEmail(String email);
